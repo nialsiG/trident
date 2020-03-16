e08f9e0b9b20e6b7bb96067144814b838591f11b
nialsiG <ghislain.thiery@ntymail.com>
2020-03-16 09:29:19 +0100

    Added Rfiles for analyze, collect, data, visualize

---
+++ R/analyze.R
@@ -0,0 +1,56 @@
+# dustyboxcox----
+#' @title dustyboxcox
+#' @description Short description of fun
+#' @param x x
+#' @return the result of fun
+#' @examples
+#' #to do
+#' @export
+dustyboxcox <- function(x){
+
+
+
+}
+
+
+# multicheck----
+#' @title multicheck
+#' @description Perform various statistical checks
+#' @param df x
+#' @param y x
+#' @return the result of fun
+#' @examples
+#' #to do
+#' @export
+multicheck <- function(){
+}
+
+# rm.outliers----
+#' @title rm.outliers
+#' @description Short description of fun
+#' @param x x
+#' @return the result of fun
+#' @examples
+#' #to do
+#' @export
+rm.outliers <- function(x){
+
+
+
+}
+
+
+# topgrazr----
+#' @title topgrazr
+#' @description Short description of fun
+#' @param x x
+#' @return the result of fun
+#' @examples
+#' #to do
+#' @export
+topgrazr <- function(x, method = P){
+
+
+
+}
+
---
+++ R/data.R
@@ -0,0 +1,6 @@
+# exData----
+#' @title exData
+#' @description An example of microwear dataset
+exData <- read.csv(system.file("extdata", "data", "example-data.csv", package = "grazr"))
+usethis::use_data(exData, overwrite = TRUE)
+'exData'
