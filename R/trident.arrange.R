a91b9ce5d732d768906bc7fe13f3ddf0f538cdca
nialsiG <ghislain.thiery@ntymail.com>
2020-07-15 17:34:05 +0200

    Function 'trident.arranged' fixed, notably: ability to arrange by number of disc. groups; non-significant p-values not included in the calculation of mean hsd/lsd p-values; arithmetic and geometric mean; group priority working; recursive function for group naming and priority

--- R/trident.arrange.R
+++ R/trident.arrange.R
@@ -6,48 +6,19 @@
 #' @examples
 #' #to do
 #' @export
-trident.arrange <- function(df, y, by = "mean.hsd.p.value", gp.priority = NULL) {
-
-  # BEFORE, check data structure
+trident.arrange <- function(df, y, by = "hsd.mean.p.value", alpha = 0.05, byngr = FALSE, geomean = FALSE, gp.priority = c(1:length(levels(y)))) {
+  #check data structure
   if (is.data.frame(df) == FALSE) stop("Dataframe df should be an object of class data.frame (see 'is.data.frame')")
   if (is.factor(y) == FALSE) stop("Category variable 'y' is not a factor")
   if (length(which(apply(df, 2, FUN = is.numeric) == FALSE)) != 0) stop("All variables of dataframe 'df' should be numeric")
 
+  #changing levels of factor if gp.priority
+  y <- factor(y, levels = levels(y)[gp.priority])
+
   #Preparation of dataset: removal of Na, NaN, Inf and -Inf:
   Mydf <- data.frame(y = as.factor(y), df)
-  Mydf <- Mydf[!is.infinite(rowSums(Mydf[, -1])),]
+  Mydf <- Mydf[!is.infinite(rowSums(Mydf[, -1])), ]
   Mydf <- stats::na.omit(Mydf)
-
-  #Groups of pair-wise comparisons
-  Names <- NULL
-  Names$AB <- paste(levels(y)[1], levels(y)[2], sep = "-")
-  Names$AC <- paste(levels(y)[1], levels(y)[3], sep = "-")
-  Names$BC <- paste(levels(y)[2], levels(y)[3], sep = "-")
-  if (length(levels(y)) > 3) {
-    Names$AD <- paste(levels(y)[1], levels(y)[4], sep = "-")
-    Names$BD <- paste(levels(y)[2], levels(y)[4], sep = "-")
-    Names$CD <- paste(levels(y)[3], levels(y)[4], sep = "-")
-    if (length(levels(y)) > 4) {
-      Names$AE <- paste(levels(y)[1], levels(y)[5], sep = "-")
-      Names$BE <- paste(levels(y)[2], levels(y)[5], sep = "-")
-      Names$CE <- paste(levels(y)[3], levels(y)[5], sep = "-")
-      Names$DE <- paste(levels(y)[4], levels(y)[5], sep = "-")
-      if (length(levels(y)) > 5) {
-        Names$AF <- paste(levels(y)[1], levels(y)[6], sep = "-")
-        Names$BF <- paste(levels(y)[2], levels(y)[6], sep = "-")
-        Names$CF <- paste(levels(y)[3], levels(y)[6], sep = "-")
-        Names$DF <- paste(levels(y)[4], levels(y)[6], sep = "-")
-        Names$EF <- paste(levels(y)[5], levels(y)[6], sep = "-")
-      }
-    }
-  }
-  #Recoding gp.priority
-  if (is.null(gp.priority) == FALSE) {
-    Gp.Priority <- NULL
-    for (g in 1:length(gp.priority)){
-      Gp.Priority[g] <- Names[unlist(gp.priority[g])]
-    }
-  }
 
   #2A Compute ANOVA's F & P
   compute.aov <- function(x, y) {
@@ -56,8 +27,8 @@ trident.arrange <- function(df, y, by = "mean.hsd.p.value", gp.priority = NULL) 
     P_AOV <- Myaov$p.value
     return(c(F_AOV, P_AOV))
   }
-  AOV <- t(plyr::colwise(compute.aov)(Mydf[,-1], y = Mydf[, 1]))
-  AOV <- data.frame(variable = rownames(AOV), f.stat = AOV[, 1], aov.p.value = AOV[, 2], row.names = NULL)
+  AOV <- t(plyr::colwise(compute.aov)(Mydf[, -1], y = Mydf[, 1]))
+  colnames(AOV) <- c("f.stat", "aov.p.value")
 
   #2B Compute Kruskal's K & P
   compute.ktest <- function(x, y) {
@@ -67,129 +38,72 @@ trident.arrange <- function(df, y, by = "mean.hsd.p.value", gp.priority = NULL) 
     return(c(K_KTEST, P_KTEST))
   }
   KTEST <- t(plyr::colwise(compute.ktest)(Mydf[,-1], y = Mydf[, 1]))
-  KTEST <- data.frame(variable = rownames(KTEST), k = KTEST[, 1], k.p.value = KTEST[, 2], row.names = NULL)
+  colnames(KTEST) <- c("k", "k.p.value")
 
   #2C Compute HSD's p-value (using package 'DescTools')
   compute.hsd <- function(x, y){
     Myhsd <- DescTools::PostHocTest(x = stats::aov(x ~ y), method = "hsd", conf.level = NA)
-    P_HSD <- NULL
-    P_HSD$ab <- Myhsd$y[1, 1]
-    P_HSD$ac <- Myhsd$y[2, 1]
-    P_HSD$bc <- Myhsd$y[2, 2]
-    if (length(levels(y)) > 3) {
-      P_HSD$ad <- Myhsd$y[3, 1]
-      P_HSD$bd <- Myhsd$y[3, 2]
-      P_HSD$cd <- Myhsd$y[3, 3]
-      if (length(levels(y)) > 4) {
-        P_HSD$ae <- Myhsd$y[4, 1]
-        P_HSD$be <- Myhsd$y[4, 2]
-        P_HSD$ce <- Myhsd$y[4, 3]
-        P_HSD$de <- Myhsd$y[4, 4]
-        if (length(levels(y)) > 5) {
-          P_HSD$af <- Myhsd$y[5, 1]
-          P_HSD$bf <- Myhsd$y[5, 2]
-          P_HSD$cf <- Myhsd$y[5, 3]
-          P_HSD$df <- Myhsd$y[5, 4]
-          P_HSD$ef <- Myhsd$y[5, 5]
-        }
-      }
-    }
-    return(unlist(P_HSD))
+    Myhsd <- c(stats::na.omit(unlist(Myhsd)))
+    mean.signif.hsd <- mean(Myhsd[which(Myhsd < alpha)])
+    geomean.signif.hsd <- exp(mean(log(Myhsd[which(Myhsd < alpha)])))
+    number.sign.grp <- length(Myhsd[which(Myhsd < alpha)])
+    return(c(Myhsd, mean.signif.hsd, geomean.signif.hsd, number.sign.grp))
   }
   HSD <- t(plyr::colwise(compute.hsd)(Mydf[, -1], y = Mydf[, 1]))
-  HSD <- data.frame(variable = rownames(HSD), HSD, row.names = NULL)
-  colnames(HSD) <- c("variable", paste0("hsd.p.", Names, sep = ""))
-  HSD$mean.hsd.p.value <- apply(HSD[, -1], 1, mean)
-
 
   #2D Compute LSD's p-value (using package 'DescTools')
   compute.lsd <- function(x, y){
     Mylsd <- DescTools::PostHocTest(x = stats::aov(x ~ y), method = "lsd", conf.level = NA)
-    P_LSD <- NULL
-    P_LSD$ab <- Mylsd$y[1, 1]
-    P_LSD$ac <- Mylsd$y[2, 1]
-    P_LSD$bc <- Mylsd$y[2, 2]
-    if (length(levels(y)) > 3) {
-      P_LSD$ad <- Mylsd$y[3, 1]
-      P_LSD$bd <- Mylsd$y[3, 2]
-      P_LSD$cd <- Mylsd$y[3, 3]
-      if (length(levels(y)) > 4) {
-        P_LSD$ae <- Mylsd$y[4, 1]
-        P_LSD$be <- Mylsd$y[4, 2]
-        P_LSD$ce <- Mylsd$y[4, 3]
-        P_LSD$de <- Mylsd$y[4, 4]
-        if (length(levels(y)) > 5) {
-          P_LSD$af <- Mylsd$y[5, 1]
-          P_LSD$bf <- Mylsd$y[5, 2]
-          P_LSD$cf <- Mylsd$y[5, 3]
-          P_LSD$df <- Mylsd$y[5, 4]
-          P_LSD$ef <- Mylsd$y[5, 5]
-        }
-      }
-    }
-    return(unlist(P_LSD))
+    Mylsd <- c(stats::na.omit(unlist(Mylsd)))
+    mean.signif.lsd <- mean(Mylsd[which(Mylsd < alpha)])
+    geomean.signif.lsd <- exp(mean(log(Mylsd[which(Mylsd < alpha)])))
+    number.sign.grp <- length(Mylsd[which(Mylsd < alpha)])
+    return(c(Mylsd, mean.signif.lsd, geomean.signif.lsd, number.sign.grp))
   }
   LSD <- t(plyr::colwise(compute.lsd)(Mydf[, -1], y = Mydf[, 1]))
-  LSD <- data.frame(variable = rownames(LSD), LSD, row.names = NULL)
-  colnames(LSD) <- c("variable", paste0("lsd.p.", Names, sep = ""))
-  LSD$mean.lsd.p.value <- apply(LSD[, -1], 1, mean)
 
-  #3 Group all in a big table
-  Mytable <- dplyr::inner_join(AOV, KTEST, by = "variable")
-  Mytable <- dplyr::inner_join(Mytable, HSD, by = "variable")
-  Mytable <- dplyr::inner_join(Mytable, LSD, by = "variable")
-
-  #4 Arrangement factor
-  Argmt.factor <- NULL
-  for (h in 1:length(by)) {
-    if (by[h] == "k") Argmt.factor <- c(Argmt.factor, 'k')
-    if (by[h] == "k.p.value") Argmt.factor <- c(Argmt.factor, 'k.p.value')
-    if (by[h] == "f.stat") Argmt.factor <- c(Argmt.factor, 'f.stat')
-    if (by[h] == "aov.p.value") Argmt.factor <- c(Argmt.factor, 'aov.p.value')
-    if (by[h] == "hsd.p.value") {
-      if (is.null(gp.priority) == TRUE) Argmt.factor <- c(Argmt.factor, colnames(HSD[, -1]))
-      else if (is.null(gp.priority) == FALSE) {
-        vec.tmp <- NULL
-        for (i in 1:length(Gp.Priority)) {
-          vec.tmp[i] <- paste("hsd.p.", Gp.Priority[i], sep = "")
-        }
-        Argmt.factor <- c(Argmt.factor,  vec.tmp)
-      }
-    }
-    if (by[h] == "lsd.p.value") {
-      if (is.null(gp.priority) == TRUE) Argmt.factor <- c(Argmt.factor, colnames(LSD[, -1]))
-      else if (is.null(gp.priority) == FALSE) {
-        vec.tmp <- NULL
-        for (j in 1:length(Gp.Priority)) {
-          vec.tmp[j] <- paste("lsd.p.", Gp.Priority[j], sep = "")
-        }
-        Argmt.factor <- c(Argmt.factor,  vec.tmp)
-      }
-    }
-    if (by[h] == "mean.hsd.p.value") Argmt.factor <- c(Argmt.factor, "mean.hsd.p.value")
-    if (by[h] == "mean.lsd.p.value") Argmt.factor <- c(Argmt.factor, "mean.lsd.p.value")
+  # 3 Groupnames
+  Groupnames <- NULL
+  for (i in c(1:length(levels(y)) - 1)) {
+    Right <- levels(y)[-(0:i)]
+    Left <- rep(levels(y)[i], length(Right))
+    Groupnames <- c(Groupnames, paste0(Left, ".vs.", Right))
   }
+  # ...note: we have got to remove the first element of vector 'Groupnames' (".vs.")
+  Groupnames <- Groupnames[-1]
+  # ...then give column names to HSD and LSD
+  colnames(HSD) <- paste0("hsd.", c(Groupnames, "mean", "geomean", "signif.groups"))
+  colnames(LSD) <- paste0("lsd.", c(Groupnames, "mean", "geomean", "signif.groups"))
 
-  #5 Arrange variables by selected method (grouped in Argmt.factor)
-  #Mynewtable <- plyr::arrange(Mytable, paste(Argmt.factor, collapse =", "))
-  if (length(Argmt.factor) == 1) Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]])
-  if (length(Argmt.factor) == 1 & Argmt.factor[1] == "k") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]))
-  if (length(Argmt.factor) == 1 & Argmt.factor[1] == "f.stat") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]))
-
-  if (length(Argmt.factor) == 2) Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], Mytable[, Argmt.factor[2]])
-  if (length(Argmt.factor) == 2 & Argmt.factor[1] == "k") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]), Mytable[, Argmt.factor[2]])
-  if (length(Argmt.factor) == 2 & Argmt.factor[1] == "f.stat") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]), Mytable[, Argmt.factor[2]])
-  if (length(Argmt.factor) == 2 & Argmt.factor[2] == "k") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], plyr::desc(Mytable[, Argmt.factor[2]]))
-  if (length(Argmt.factor) == 2 & Argmt.factor[2] == "f.stat") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], plyr::desc(Mytable[, Argmt.factor[2]]))
-
-  if (length(Argmt.factor) >= 3) Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], Mytable[, Argmt.factor[2]], Mytable[, Argmt.factor[3]])
-  if (length(Argmt.factor) >= 3 & Argmt.factor[1] == "k") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]), Mytable[, Argmt.factor[2]], Mytable[, Argmt.factor[3]])
-  if (length(Argmt.factor) >= 3 & Argmt.factor[1] == "f.stat") Mynewtable <- plyr::arrange(Mytable, plyr::desc(Mytable[, Argmt.factor[1]]), Mytable[, Argmt.factor[2]], Mytable[, Argmt.factor[3]])
-  if (length(Argmt.factor) >= 3 & Argmt.factor[2] == "k") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], plyr::desc(Mytable[, Argmt.factor[2]]), Mytable[, Argmt.factor[3]])
-  if (length(Argmt.factor) >= 3 & Argmt.factor[2] == "f.stat") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], plyr::desc(Mytable[, Argmt.factor[2]]), Mytable[, Argmt.factor[3]])
-  if (length(Argmt.factor) >= 3 & Argmt.factor[3] == "k") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], Mytable[, Argmt.factor[2]], plyr::desc(Mytable[, Argmt.factor[3]]))
-  if (length(Argmt.factor) >= 3 & Argmt.factor[3] == "f.stat") Mynewtable <- plyr::arrange(Mytable, Mytable[, Argmt.factor[1]], Mytable[, Argmt.factor[2]], plyr::desc(Mytable[, Argmt.factor[3]]))
-
+  #4 Arrange by
+  # ...group all in a big table
+  Mytable <- data.frame(AOV, KTEST, HSD, LSD)
+  # ...now arrange by factor
+  # ......aov
+  if (by == "f.stat") Mynewtable <- Mytable[with(Mytable, order(f.stat, decreasing = TRUE)), ]
+  if (by == "aov.p.value") Mynewtable <- Mytable[with(Mytable, order(aov.p.value, decreasing = FALSE)), ]
+  # ......k
+  if (by == "k") Mynewtable <- Mytable[with(Mytable, order(k, decreasing = TRUE)), ]
+  if (by == "k.p.value") Mynewtable <- Mytable[with(Mytable, order(k.p.value, decreasing = FALSE)), ]
+  # ......hsd
+  if (by == "hsd.p.value") Mynewtable <- Mytable[with(Mytable, order(Mytable[, which(colnames(Mytable) == "k.p.value") + 1],
+                                                                     Mytable[, which(colnames(Mytable) == "k.p.value") + 2],
+                                                                     decreasing = c(FALSE, FALSE))), ]
+  if (by == "hsd.mean.p.value") {
+    if (byngr == FALSE & geomean == FALSE) Mynewtable <- Mytable[with(Mytable, order(hsd.mean, decreasing = FALSE)), ]
+    if (byngr == TRUE & geomean == FALSE) Mynewtable <- Mytable[with(Mytable, order(hsd.signif.groups, hsd.mean, decreasing = c(TRUE, FALSE))), ]
+    if (byngr == FALSE & geomean == TRUE) Mynewtable <- Mytable[with(Mytable, order(hsd.geomean, decreasing = FALSE)), ]
+    if (byngr == TRUE & geomean == TRUE) Mynewtable <- Mytable[with(Mytable, order(hsd.signif.groups, hsd.geomean, decreasing = c(TRUE, FALSE))), ]
+  }
+  # ......lsd
+  if (by == "lsd.p.value") Mynewtable <- Mytable[with(Mytable, order(Mytable[, which(colnames(Mytable) == "hsd.signif.groups") + 1],
+                                                                     Mytable[, which(colnames(Mytable) == "hsd.signif.groups") + 2],
+                                                                     decreasing = c(FALSE, FALSE))), ]
+  if (by == "lsd.mean.p.value") {
+    if (byngr == FALSE & geomean == FALSE) Mynewtable <- Mytable[with(Mytable, order(lsd.mean, decreasing = FALSE)), ]
+    if (byngr == TRUE & geomean == FALSE) Mynewtable <- Mytable[with(Mytable, order(lsd.signif.groups, lsd.mean, decreasing = c(TRUE, FALSE))), ]
+    if (byngr == FALSE & geomean == TRUE) Mynewtable <- Mytable[with(Mytable, order(lsd.geomean, decreasing = FALSE)), ]
+    if (byngr == TRUE & geomean == TRUE) Mynewtable <- Mytable[with(Mytable, order(lsd.signif.groups, lsd.geomean, decreasing = c(TRUE, FALSE))), ]
+  }
   return(Mynewtable)
 }
-
